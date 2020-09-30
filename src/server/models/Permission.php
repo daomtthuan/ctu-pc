ea9377b622551032aacb38d6d1b1333718231023
b1704735 <53897463+b1704735@users.noreply.github.com>
2020-09-30 23:35:24 +0700

    [30-09-20] add 3 models
    - user
    - category
    - permission

---
+++ src/server/models/Permission.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Models;
+class Permission{
+    private int $id;
+    private int $idUser;
+    private int $idRole;
+    private bool $state;
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get the value of idUser
+     */ 
+    public function getIdUser()
+    {
+        return $this->idUser;
+    }
+
+    /**
+     * Get the value of idRole
+     */ 
+    public function getIdRole()
+    {
+        return $this->idRole;
+    }
+
+    /**
+     * Get the value of state
+     */ 
+    public function getState()
+    {
+        return $this->state;
+    }
+}
