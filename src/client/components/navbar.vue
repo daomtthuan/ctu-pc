943d7020b68fe4843420c72e548c044824896ea8
Daomtthuan <dao.mt.thuan@gmail.com>
2020-10-05 09:27:25 +0700

    [05-10-2020]: fix bug navbar

--- src/client/components/navbar.vue
+++ src/client/components/navbar.vue
@@ -1,68 +1,83 @@
 <template>
   <section>
-    <b-navbar toggleable="md" type="dark" variant="primary">
-      <b-navbar-brand to="/">CTU PC SHOP</b-navbar-brand>
+    <b-navbar toggleable="lg" type="dark" variant="primary" sticky>
+      <b-navbar-brand to="/" tag="h1" class="mb-0">CTU PC SHOP</b-navbar-brand>
 
-      <b-button v-b-toggle.sidebar variant="light" class="d-md-none">
+      <b-button v-b-toggle.sidebar variant="light" class="d-lg-none">
         <fa :icon="['fas', 'bars']"></fa>
       </b-button>
 
-      <b-collapse id="nav-collapse" is-nav>
+      <b-collapse is-nav>
         <b-navbar-nav>
           <b-nav-item>Trang chủ</b-nav-item>
-          <b-nav-item-dropdown text="Danh mục sản phẩm">
-            <b-dropdown-group v-for="(categoryGroup, index) in categoryGroups" :key="categoryGroup.id" :header="categoryGroup.name">
-              <b-dropdown-item-button v-for="category in categories[index]" :key="category.id">
-                {{ category.name }}
-              </b-dropdown-item-button>
-              <b-dropdown-divider v-if="index < categoryGroups.length - 1"></b-dropdown-divider>
-            </b-dropdown-group>
+          <b-nav-item-dropdown text="Danh mục sản phẩm" no-caret menu-class="accordion p-0 border-0" role="tablist">
+            <b-card v-for="(categoryGroup, index) in categoryGroups" :key="categoryGroup.id" no-body style="min-width: 200px;">
+              <b-card-header header-tag="header" class="p-1" role="tab">
+                <b-button v-b-toggle="`category-group-${categoryGroup.id}`" block variant="primary" class="text-left">
+                  {{ categoryGroup.name }}
+                </b-button>
+              </b-card-header>
+
+              <b-collapse :id="`category-group-${categoryGroup.id}`" accordion="category-group-accordion" role="tabpanel">
+                <b-card-body class="px-1 py-0">
+                  <b-dropdown-item-button v-for="category in categories[index]" :key="category.id" class="my-1">
+                    {{ category.name }}
+                  </b-dropdown-item-button>
+                </b-card-body>
+              </b-collapse>
+            </b-card>
           </b-nav-item-dropdown>
           <b-nav-item>Tin tức - Sự kiện</b-nav-item>
           <b-nav-item>Chính sách - Hướng dẫn</b-nav-item>
         </b-navbar-nav>
-
-        <!-- Right aligned nav items -->
-        <b-navbar-nav class="ml-auto">
-          <b-nav-form>
-            <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-            <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-          </b-nav-form>
-
-          <b-nav-item-dropdown text="Lang" right>
-            <b-dropdown-item href="#">EN</b-dropdown-item>
-            <b-dropdown-item href="#">ES</b-dropdown-item>
-            <b-dropdown-item href="#">RU</b-dropdown-item>
-            <b-dropdown-item href="#">FA</b-dropdown-item>
-          </b-nav-item-dropdown>
-
-          <b-nav-item-dropdown right>
-            <!-- Using 'button-content' slot -->
-            <template v-slot:button-content>
-              <em>User</em>
-            </template>
-            <b-dropdown-item href="#">Profile</b-dropdown-item>
-            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-          </b-nav-item-dropdown>
-        </b-navbar-nav>
       </b-collapse>
     </b-navbar>
 
-    <b-sidebar id="sidebar" tag="nav" shadow backdrop>
-      <template slot="title">
-        <b-navbar-brand to="/">CTU PC SHOP</b-navbar-brand>
+    <b-sidebar id="sidebar" shadow backdrop no-header sticky>
+      <template v-slot:default="{ hide }">
+        <b-navbar>
+          <b-navbar-brand to="/" tag="h1" class="mb-0">CTU PC SHOP</b-navbar-brand>
+
+          <b-button variant="danger" @click="hide" class="ml-auto">
+            <fa :icon="['fas', 'times']"></fa>
+          </b-button>
+        </b-navbar>
+
+        <div class="px-3">
+          <b-button block variant="primary" class="text-left my-2">
+            Trang chủ
+          </b-button>
+
+          <b-button v-b-toggle.category-group block variant="primary" class="text-left my-2">
+            Danh mục sản phẩm
+          </b-button>
+
+          <b-collapse id="category-group" class="accordion my-2">
+            <b-card v-for="(categoryGroup, index) in categoryGroups" :key="categoryGroup.id" no-body>
+              <b-card-header header-tag="header" class="p-1" role="tab">
+                <b-button v-b-toggle="`category-group-${categoryGroup.id}`" block variant="light" class="text-left">
+                  {{ categoryGroup.name }}
+                </b-button>
+              </b-card-header>
+
+              <b-collapse :id="`category-group-${categoryGroup.id}`" accordion="category-group-accordion" role="tabpanel">
+                <b-card-body class="px-1 py-0">
+                  <b-button v-for="category in categories[index]" :key="category.id" block class="text-dark text-left border-0 rounded-0 my-1" variant="outline-light">
+                    {{ category.name }}
+                  </b-button>
+                </b-card-body>
+              </b-collapse>
+            </b-card>
+          </b-collapse>
+
+          <b-button block variant="primary" class="text-left my-2">
+            Tin tức - Sự kiện
+          </b-button>
+          <b-button block variant="primary" class="text-left my-2">
+            Chính sách - Hướng dẫn
+          </b-button>
+        </div>
       </template>
-      <template slot="header-close">
-        <b-button variant="danger">
-          <fa :icon="['fas', 'times']"></fa>
-        </b-button>
-      </template>
-      <div class="px-3 py-2">
-        <p>
-          Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-        </p>
-        <b-img src="https://picsum.photos/500/500/?image=54" fluid thumbnail></b-img>
-      </div>
     </b-sidebar>
   </section>
 </template>
@@ -72,8 +87,8 @@
 
   @Component
   export default class AppNavbar extends Vue {
-    @Prop(Object) categoryGroups!: App.Models.CategoryGroup[];
-    @Prop(Object) categories!: App.Models.Category[][];
+    @Prop(Array) categoryGroups!: App.Models.CategoryGroup[];
+    @Prop(Array) categories!: App.Models.Category[][];
   }
 </script>
 
