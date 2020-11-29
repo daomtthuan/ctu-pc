f1c3a6eb1693d6f2eeb2d6a115715a686e153c51
b1704735 <53897463+b1704735@users.noreply.github.com>
2020-11-29 20:02:10 +0700

    [29-11-2020] add function for CategoryGroupApi
    - function : post, put, delete
    - add function CategoryGroupProvider : create, edit, remove

--- src/server/api/CategoryGroupApi.php
+++ src/server/api/CategoryGroupApi.php
@@ -3,8 +3,10 @@
 namespace Api;
 
 use Core\Api;
+use Core\Request;
 use Core\Response;
 use Provider\CategoryGroupProvider;
+use Entity\CategoryGroup;
 
 /** CategoryGroup api */
 class CategoryGroupApi extends Api {
@@ -16,5 +18,61 @@ class CategoryGroupApi extends Api {
     Response::getInstance()->sendJson(CategoryGroupProvider::find([
       'state' => 1
     ]));
+  }
+
+  public static function post() {
+    Request::getInstance()->verifyAdminAccount();
+
+    if (!Request::getInstance()->hasData('name')) {
+      Response::getInstance()->sendStatus(400);
+    }
+
+    $success = CategoryGroupProvider::create(new CategoryGroup([
+      'id' => null,
+      'name' => Request::getInstance()->getData('name'),
+      'state' => null
+    ]));
+
+    Response::getInstance()->sendStatus($success ? 200 : 500);
+  }
+
+  public static function put() {
+    Request::getInstance()->verifyAdminAccount();
+
+    if (!Request::getInstance()->hasParam('id') || !Request::getInstance()->hasData('name', 'state')) {
+      Response::getInstance()->sendStatus(400);
+    }
+
+    $CategoryGroups = CategoryGroupProvider::find([
+      'id' => Request::getInstance()->getParam('id')
+    ]);
+    if (count($CategoryGroups) != 1) {
+      Response::getInstance()->sendStatus(404);
+    }
+
+    $CategoryGroups[0]->setName(Request::getInstance()->getData('name'));
+    $CategoryGroups[0]->setState(Request::getInstance()->getData('state'));
+
+    CategoryGroupProvider::edit($CategoryGroups[0]);
+    Response::getInstance()->sendStatus(200);
+  }
+
+  public static function delete() {
+    Request::getInstance()->verifyAdminAccount();
+
+    if (!Request::getInstance()->hasParam('id')) {
+      Response::getInstance()->sendStatus(400);
+    }
+
+    $CategoryGroups = CategoryGroupProvider::find([
+      'id' => Request::getInstance()->getParam('id')
+    ]);
+    if (count($CategoryGroups) != 1) {
+      Response::getInstance()->sendStatus(404);
+    }
+
+    $success = CategoryGroupProvider::remove($CategoryGroups[0]->getId());
+
+    Response::getInstance()->sendStatus($success ? 200 : 500);
   }
 };
