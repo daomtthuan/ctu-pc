f86f4a66d7507880bb47b830e84c60222522e1c5
b1704735 <53897463+b1704735@users.noreply.github.com>
2020-12-01 01:35:34 +0700

    Revert "[29-11-2020] add function for CategoryGroupApi"
    This reverts commit f1c3a6eb1693d6f2eeb2d6a115715a686e153c51.

--- src/server/provider/CategoryGroupProvider.php
+++ src/server/provider/CategoryGroupProvider.php
@@ -4,7 +4,6 @@ namespace Provider;
 
 use Core\Database;
 use Entity\CategoryGroup;
-use Exception;
 
 /** CategoryGroup provider */
 class CategoryGroupProvider {
@@ -21,55 +20,5 @@ class CategoryGroupProvider {
       $categoryGroups[] = new CategoryGroup($data);
     }
     return $categoryGroups;
-  }
-
-  /**
-   * Create CategoryGroup
-   * 
-   * @param CategoryGroup $CategoryGroup Created CategoryGroup
-   *
-   * @return bool True if success, otherwise false
-   */
-  public static function create(CategoryGroup $CategoryGroup) {
-    $data = $CategoryGroup->jsonSerialize();
-    unset($data['id'], $data['state']);
-    return Database::getInstance()->create('CategoryGroup', $data) > 0;
-  }
-
-  /**
-   * Edit CategoryGroup
-   * 
-   * @param CategoryGroup $CategoryGroup Edited CategoryGroup
-   * 
-   * @return bool True if success, otherwise false
-   */
-  public static function edit(CategoryGroup $CategoryGroup) {
-    $data = $CategoryGroup->jsonSerialize();
-    unset($data['id']);
-    return Database::getInstance()->edit('CategoryGroup', $CategoryGroup->getId(), $data) == 1;
-  }
-
-  /**
-   * Remove CategoryGroup
-   * 
-   * @param int $id Id CategoryGroup
-   * 
-   * @return bool True if success, otherwise false
-   */
-  public static function remove(int $id) {
-    return Database::getInstance()->doTransaction(function ($id) {
-      $referenceFilters = [
-        Database::getInstance()->createReferenceFilter('CategoryGroup', 'id', $id)
-      ];
-      Database::getInstance()->removeInJoin('Product', 'idCategory', true, [
-        Database::getInstance()->createReference('Category', 'Category', 'id', 'Product', 'idCategory', Database::JOIN_INNER),
-        Database::getInstance()->createReference('CategoryGroup', 'CategoryGroup', 'id', 'Category', 'idCategoryGroup', Database::JOIN_INNER),
-      ], $referenceFilters);
-
-      Database::getInstance()->remove('Category', ['idCategoryGroup' => $id]);
-      if (Database::getInstance()->remove('CategoryGroup', ['id' => $id]) != 1) {
-        throw new Exception("Not found CategoryGroup");
-      }
-    }, $id);
   }
 }
